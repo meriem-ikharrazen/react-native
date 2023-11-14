d1ae6c50184c6d344002d446443e70e005d7f406
Meriem Ikharrazen <meriem.ikharrazen@gmail.com>
2023-11-14 16:22:50 +0100

    icons & flexbox

--- App.js
+++ App.js
@@ -69,8 +69,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
     marginTop: 20,
+    flex: 1,
   },
 });
--- components/todo-item.js
+++ components/todo-item.js
@@ -1,12 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function TodoItem({ item, pressHandler }) {
   return (
-    <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      {item.text && <Text style={styles.item}>{item.text}</Text>}
-    </TouchableOpacity>
+    <View style={styles.item}>
+      <Text>{item.text}</Text>
+      <TouchableOpacity onPress={() => pressHandler(item.key)}>
+        <MaterialIcons
+          name="delete"
+          size={24}
+          color="black"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -18,5 +27,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
